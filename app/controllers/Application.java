93d19a608cd9bdf3320c57fbfdb662806baba43d
Coulibaly Mamadou <seyuf@hotmail.com>
2015-02-09 06:34:38 +0000

    google parser and printing implemented, facebook api incoming

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -31,46 +31,32 @@ public class Application extends Controller {
         }
         userInfo.saveBasicAttrToDom();
         //google async parser
-        F.Promise<JsonNode> googlePromise = WS.url("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" +  encode_query + "&rsz=8")
+        F.Promise<JsonNode> googlePromisePageOne = WS.url("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" +  encode_query + "&rsz=8&start=1")
+                .get()
+                .map(GoogleSearcher.SearchOnGoogle(userInfo));
+        F.Promise<JsonNode> googlePromisePageTwo = WS.url("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" +  encode_query + "&rsz=8&start=5")
                 .get()
                 .map(GoogleSearcher.SearchOnGoogle(userInfo));
         //facebook async parser incoming
 
         //list of parsers promises
-        return F.Promise.sequence(googlePromise).flatMap(new F.Function<List<JsonNode>, F.Promise<Result>>(){
+        return F.Promise.sequence(googlePromisePageOne, googlePromisePageTwo).flatMap(new F.Function<List<JsonNode>, F.Promise<Result>>() {
             @Override
             public F.Promise<Result> apply(List<JsonNode> jsonNodes) throws Throwable {
-
-                System.out.println("Result Test ==================++++++");
-
-
-
                 List<String> urls = jsonNodes.get(0).findValuesAsText("url");
-                System.out.println("google count: "+ urls.size());
-                int[] i={0};
+                urls.addAll(jsonNodes.get(1).findValuesAsText("url"));
+                int[] i = {0};
                 List<F.Promise<JsonNode>> listOfPromise = new ArrayList<F.Promise<JsonNode>>();
                 urls.forEach((url) -> {
-                    System.out.println("\n\n*********Entering *********" + i[0] + ": " + url);
-                    // GoogleSearcher.parseHtml(url, curUser);
                     listOfPromise.add(WS.url(url).get().map(GoogleSearcher.parseHtml(url, userInfo, jsonNodes.get(0))));
                     i[0]++;
                 });
-                System.out.println("***end of parsing****");
-                F.Promise<JsonNode>[] toto = new F.Promise[8];
-                toto[0] = listOfPromise.get(0);
-                toto[1] = listOfPromise.get(1);
-                toto[2] = listOfPromise.get(2);
-                toto[3] = listOfPromise.get(3);
-                toto[4] = listOfPromise.get(4);
-                toto[5] = listOfPromise.get(5);
-                toto[6] = listOfPromise.get(6);
-                toto[7] = listOfPromise.get(7);
-               return F.Promise.sequence(listOfPromise).map(new F.Function<List<JsonNode>, Result>() {
-                      @Override
-                      public Result apply(List<JsonNode> jsonNodes) throws Throwable {
-                          return ok(play.libs.Json.parse(userInfo.jsonUser()));
-                      }
-                  });
+                return F.Promise.sequence(listOfPromise).map(new F.Function<List<JsonNode>, Result>() {
+                    @Override
+                    public Result apply(List<JsonNode> jsonNodes) throws Throwable {
+                        return ok(play.libs.Json.parse(userInfo.jsonUser()));
+                    }
+                });
             }
         });
     }
--- app/services/crawlers/GoogleSearcher.java
+++ app/services/crawlers/GoogleSearcher.java
@@ -8,29 +8,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import play.api.libs.json.Json;
 import play.libs.F;
-import play.libs.XML;
-import play.libs.XPath;
-import play.libs.ws.WSRequestHolder;
 import play.libs.ws.*;
-import play.libs.F.Promise;
-import scala.Function1;
-import scala.collection.immutable.*;
-import scala.concurrent.ExecutionContext;
-import scala.concurrent.Future;
-import scala.runtime.BoxedUnit;
 import services.persons.XUser;
 
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-import static akka.dispatch.Futures.sequence;
-//import scala.concurrent.ExecutionContext.Implicits.*;
 
 /**
  * Created by madalien on 31/01/15.
@@ -43,49 +24,14 @@ public class GoogleSearcher {
 
     public static  F.Function<WSResponse, JsonNode> SearchOnGoogle(XUser curUser){
         return new F.Function<WSResponse, JsonNode>(){
-
             @Override
             public JsonNode apply(WSResponse response) throws Throwable {
                 return response.asJson();
-
-
-
-
-                /*
-
-                java.lang.Iterable<scala.concurrent.Future<Integer>> listOfFutureInts = new ArrayList<scala.concurrent.Future<Integer>>();
-
-                Future<Iterable<Integer>> futureListOfInts = sequence(listOfFutureInts, global);
-                Future<Long> futureSum = futureListOfInts.map(
-                        new Mapper<Iterable<Integer>, Long>() {
-                            public Long apply(Iterable<Integer> ints) {
-                                long sum = 0;
-                                for (Integer i : ints)
-                                    sum += i;
-                                return sum;
-                            }
-                        }, global);
-
-                futureSum.onSuccess(new OnSuccess<Long>() {
-                    @Override
-                    public void onSuccess(Long result) throws Throwable {
-                        System.out.println(futureSum);
-                    }
-                }, global);*/
             }
         };
     }
 
     public static F.Function<WSResponse, JsonNode> parseHtml(String htmlUrl, XUser curUser, JsonNode nodeIn){
-        /*** Incoming Use ws instead of Jsoup for performance purposes async sequence of arrays
-         *  org.jsoup.nodes.Document html = null;
-                try {
-                    html = Jsoup.connect(htmlUrl).get();
-                } catch (IOException e) {
-                    System.out.println("Soup error"+ e.getMessage());
-                    return (JsonNode)Json.parse("{\"toto\":\"toto\"}");
-                }
-         * */
         return new F.Function<WSResponse, JsonNode>(){
             @Override
             public JsonNode apply(WSResponse response) throws Throwable {
